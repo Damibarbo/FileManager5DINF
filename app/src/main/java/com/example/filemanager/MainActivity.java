39fb53a3b7f527b0a531901040f3748676eb6c98
damibarbo <damibarbo@gmail.com>
2022-02-16 17:23:39 +0100

    gestione file 16/02/2022

---
+++ app/src/main/java/com/example/filemanager/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.filemanager;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Context;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView testo;
+    Button leggere, scrivere;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        testo = findViewById(R.id.testo);
+        leggere = findViewById(R.id.btnLeggi);
+        scrivere = findViewById(R.id.btnScrivi);
+        GestioneFile gf = new GestioneFile();
+        Context context = getApplicationContext();
+
+        leggere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                testo.setText(gf.leggiFile("Test.txt", getApplicationContext()));
+            }
+        });
+
+        scrivere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = gf.scriviFile("Test.txt", getApplicationContext());
+                Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
+
